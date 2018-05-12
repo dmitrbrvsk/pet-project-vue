73fba20475f3133112a19d4e1b48635246b800ee
Dmitrij Borovskij <dmitr.borovskij@gmail.com>
2018-05-13 02:33:19 +0300

    add autoprefixer && postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,14 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  { loader: 'css-loader', options: { importLoaders: 1 } },
+                  'postcss-loader'
+                ]
             }
         ],
     },
