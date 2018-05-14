ccad6374da6f8702cf6cb87bd75834f4556cf15c
Dmitrij Borovskij <dmitr.borovskij@gmail.com>
2018-05-14 03:25:08 +0300

    hotfix styles page 404

--- src/components/pages/NotFoundPage.vue
+++ src/components/pages/NotFoundPage.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1 class='not-found'>404 Not Found</h1>
+  <div class='page-not-found'>
+    <h1 class='page-not-found__title'>404 Not Found</h1>
+    <img class='page-not-found__image' src='/images/404.jpg' width='250' height='250' />
+    <router-link  :to="{ name: 'home'}" class='page-not-found__text'>Вернуться на главную</router-link>
+  </div>
 </template>
 
 <script>
   export default {}
 </script>
 
-<style>
-  .not-found {
-    color: brown;
+<style lang='scss' scoped>
+  .page-not-found {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+
+    &__title {
+      color: brown;
+    }
+
+    &__image {
+      object-fit: cover;
+      border-radius: 50%;
+      margin-bottom: 30px;
+    }
   }
 </style>
