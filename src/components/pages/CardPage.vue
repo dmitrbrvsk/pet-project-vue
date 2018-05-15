ac51d9de72823efb6318be87f91ec23d34fbb38c
Dmitrij Borovskij <dmitr.borovskij@gmail.com>
2018-05-15 11:39:20 +0300

    fix style CardPage component

--- src/components/pages/CardPage.vue
+++ src/components/pages/CardPage.vue
@@ -1,13 +1,14 @@
 <template>
   <div class='card-page'>
     <div v-if='card' class='card-page__content'>
-        <h1>{{ card.id }}</h1>
-        <div class='card-page__title'>{{ card.title }}</div>
-        <div class='card-page__description'>{{ card.description }}</div>
-        <div class='card-page__text'>{{ card.text }}</div>
-        <router-link :to="{ name: 'home' }" class='card-page__link-back'>На главную</router-link>
-      </div>
-      <Jumper v-if='!card'></Jumper>
+      <div class='card-page__number'>Номер карточки: {{ card.id }}</div>
+      <div class='card-page__title'>{{ card.title }}</div>
+      <div class='card-page__description'>{{ card.description }}</div>
+      <div class='card-page__text'>{{ card.text }}</div>
+      <img :src='card.image_url' class='card-page__image' />
+      <router-link :to="{ name: 'home' }" class='card-page__link-back'>На главную</router-link>
+    </div>
+    <Jumper v-if='!card'></Jumper>
   </div>
 </template>
 
@@ -39,6 +40,43 @@
   }
 </script>
 
-<style>
+<style lang='scss' scoped>
+  .card-page {
+    &__content {
+      max-width: 800px;
+      margin-top: 30px;
+      padding: 15px;
+      border: 1px solid #e4e4ef;
+
+      @media only screen and (max-width: 480px) {
+        margin-top: 10px;
+      }
+    }
+
+    &__number {
+      margin-top: 10px;
+      text-align: center;
+    }
+
+    &__title {
+      font-size: 18px;
+    }
+
+    &__title,
+    &__text,
+    &__description {
+      margin: 10px 0;
+    }
+
+    &__image {
+      width: 100%;
+      height: auto;
+    }
+
+    &__link-back {
+      display: block;
+      text-align: center;
+    }
+  }
 </style>
 
